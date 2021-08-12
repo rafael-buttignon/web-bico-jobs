b17203150f5a6f40702dc8ccac5df9a8882a7bb3
Rafael Augusto <rafael.buttignon@programmers.com.br>
2021-08-12 15:28:34 -0300

    add services and changes the .csproject

--- ProjectFatec.Api/ProjectFatec.Api/ClientApp/src/app/app.component.ts
+++ ProjectFatec.Api/ProjectFatec.Api/ClientApp/src/app/app.component.ts
@@ -1,12 +1,23 @@
+import { SampleService } from './../share/apiService/sample.service';
 import { environment } from './../environments/environment';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   client_env_name: string = environment.env_name;
   server_env_name: string = null;
+
+  constructor(private SampleService: SampleService) {
+
+  }
+  ngOnInit(): void {
+    this.SampleService.getAppSettings().subscribe(
+      (value: string) => this.server_env_name = value,
+      (error: any) => console.error("error", error)
+    )
+  }
 }
