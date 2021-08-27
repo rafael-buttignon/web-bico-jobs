db7e8608a4ec30d9e561bb24c7a5710f81993034
Rafael Buttignon <rafaaugustocontato@gmail.com>
2021-08-27 00:29:56 -0300

    configuração inicial

---
+++ ProjectFatec.Api/Fatec.Domain/Exceptions/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Fatec.Domain.Entities
+{
+    class Class177
+    {
+        //Apenas para não dar sumir a pasta quando subir
+    }
+}
---
+++ ProjectFatec.Api/Fatec.Domain/Services/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Fatec.Domain.Entities
+{
+    class Class12
+    {
+        //Apenas para não dar sumir a pasta quando subir
+    }
+}
