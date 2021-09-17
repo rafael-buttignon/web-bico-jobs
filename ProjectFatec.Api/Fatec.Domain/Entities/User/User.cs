a601557461c190bacfc34b5427bfd41ab4928847
Jean Silva <jean.silva@programmers.com.br>
2021-09-17 20:40:00 -0300

    [PBI60] Configurações das entidades

--- ProjectFatec.Api/Fatec.Domain/Entities/User/User.cs
+++ ProjectFatec.Api/Fatec.Domain/Entities/User/User.cs
@@ -1,6 +1,21 @@
-﻿namespace Fatec.Domain.Entities.User
+﻿using System;
+using System.Collections.Generic;
+using AddressEntity = Fatec.Domain.Entities.Address.Address;
+using RequestEntity = Fatec.Domain.Entities.Request.Request;
+using ContractEntity = Fatec.Domain.Entities.Contract.Contract;
+
+namespace Fatec.Domain.Entities.User
 {
     public class User : Entity
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string CPF { get; set; }
+        public AddressEntity Address { get; set; }
+        public virtual long AddressId { get; set; }
+        public ICollection<RequestEntity> Requests { get; set; }
+        public ICollection<ContractEntity> Contracts { get; set; }
     }
 }
--- ProjectFatec.Api/Fatec.Infrastructure/Context/BicoContext.cs
+++ ProjectFatec.Api/Fatec.Infrastructure/Context/BicoContext.cs
@@ -4,6 +4,15 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Fatec.Infrastructure.Extensions;
+using Fatec.Infrastructure.ModelConfig.JobCategory;
+using Fatec.Infrastructure.ModelConfig.Address;
+using Fatec.Infrastructure.ModelConfig.Contract;
+using Fatec.Infrastructure.ModelConfig.ContractStatus;
+using Fatec.Infrastructure.ModelConfig.Job;
+using Fatec.Infrastructure.ModelConfig.Request;
+using Fatec.Infrastructure.ModelConfig.RequestStatus;
+using Fatec.Infrastructure.ModelConfig.User;
 
 namespace Fatec.Infrastructure.Context
 {
@@ -19,7 +28,17 @@ namespace Fatec.Infrastructure.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new JobCategoryConfig());
+            modelBuilder.ApplyConfiguration(new AddressConfig());
+            modelBuilder.ApplyConfiguration(new ContractConfig());
+            modelBuilder.ApplyConfiguration(new ContractStatusConfig());
+            modelBuilder.ApplyConfiguration(new JobConfig());
+            modelBuilder.ApplyConfiguration(new RequestConfig());
+            modelBuilder.ApplyConfiguration(new RequestStatusConfig());
+            modelBuilder.ApplyConfiguration(new UserConfig());
 
+            modelBuilder.AddSeeds();
+            base.OnModelCreating(modelBuilder);
         }
 
         public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
